42cba94d07411687fa49aa484fa5cd7ef95323dd
Rafael Luque Leiva <rafael.luque@osoco.es>
2022-01-18 23:24:10 +0100

    Update NodeJS version from 8 to 12

--- nodejs/build-settings.sh
+++ nodejs/build-settings.sh
@@ -1,4 +1,5 @@
-defineEnvVar NODEJS_MAJOR_VERSION MANDATORY "The major version of NodeJS" "8";
+overrideEnvVar PARENT_IMAGE_TAG "master-amd64";
+defineEnvVar NODEJS_MAJOR_VERSION MANDATORY "The major version of NodeJS" "12";
 overrideEnvVar TAG '${NODEJS_MAJOR_VERSION}';
 overrideEnvVar SERVICE_USER '${NAMESPACE}';
 overrideEnvVar ENABLE_RSNAPSHOT "false";
